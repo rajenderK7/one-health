c786fdf66b6066c6acf47a7b7b3f9a4c866a8925
Rajender <rajenderkatkuri123@gmail.com>
2022-11-04 19:20:12 +0530

    Add doctor mail and user mail to session

--- src/models/sessionModel.ts
+++ src/models/sessionModel.ts
@@ -5,11 +5,14 @@ export interface SessionModel {
   diagnosticID: string;
   userName: string;
   doctorName: string;
+  userMail: string;
+  doctorMail: string;
   symptoms: string;
   active: boolean;
   complete: number;
   meetLink: string;
   paymentLink: string;
+  consultationFee: string;
   prescriptionDownloadLink?: string;
   paymentDone: boolean;
 }
